9346014ab2ace224195b862c2932d56c28f6c779
marcosdenisalves <dee.alves2@gmail.com>
2023-02-16 01:55:24 -0300

    Creating project

---
+++ src/test/java/com/company/financialmanagement/FinancialmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.financialmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
