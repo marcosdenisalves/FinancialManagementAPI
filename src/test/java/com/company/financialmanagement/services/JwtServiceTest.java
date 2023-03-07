1d2dec8aedd7a878d2838c79a234b35d07b05d70
marcosdenisalves <dee.alves2@gmail.com>
2023-03-07 03:15:44 -0300

    Adding unit test to athenticatie

--- src/test/java/com/company/financialmanagement/services/JwtServiceTest.java
+++ src/test/java/com/company/financialmanagement/services/JwtServiceTest.java
@@ -1,25 +1,33 @@
 package com.company.financialmanagement.services;
 
+import com.company.financialmanagement.dtos.AuthenticationDTO;
 import com.company.financialmanagement.dtos.AuthenticationRegisterDTO;
+import com.company.financialmanagement.models.Token;
 import com.company.financialmanagement.models.User;
 import com.company.financialmanagement.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JwtServiceTest {
     @Autowired
     private AuthenticationService authenticationService;
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
-    private AuthenticationRegisterDTO registerDTO;
+   private AuthenticationRegisterDTO registerDTO;
+   private AuthenticationDTO authenticationDTO;
     @BeforeEach
     public void initTest() {
          registerDTO = AuthenticationRegisterDTO.builder()
@@ -28,13 +36,28 @@ public class JwtServiceTest {
                 .firstname("adm")
                 .password("1234")
                 .build();
+
+        authenticationDTO = AuthenticationDTO.builder()
+                .email("administrator@email.com")
+                .password("1234")
+                .build();
     }
 
     @Test
-    public void testIfUserHasBeenRegistered() {
-        authenticationService.register(registerDTO);
-
+    public void shouldRegisterNewUser() {
+        Token token = authenticationService.register(registerDTO);
         User user = assertDoesNotThrow(() -> userRepository.findByEmail("administrator@email.com").orElseThrow());
+
+        assertNotNull(user);
+        assertNotNull(token);
         assertEquals(registerDTO.getEmail(), user.getEmail());
+    }
+
+    @Test
+    public void shouldAuthenticateUser() {
+        authenticationService.register(registerDTO);
+        Token token = authenticationService.authenticate(authenticationDTO);
+
+        assertNotNull(token);
     }
 }
